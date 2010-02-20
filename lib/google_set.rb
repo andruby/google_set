6750debd27fb275814f5b34946723a967fa753c9
Daniel Cadenas <dcadenas@gmail.com>
2010-02-20 13:59:43 -0200

    Added exception

--- lib/google_set.rb
+++ lib/google_set.rb
@@ -1,14 +1,24 @@
 require 'open-uri'
 require 'cgi'
- 
+
 class GoogleSet
+  class Error < StandardError
+    attr_accessor :wrapped_exception
+    def initialize(ex)
+      @wrapped_exception = ex
+      set_backtrace(ex.backtrace)
+    end
+  end
+
   def self.for(*terms)
     path = "http://labs.google.com/sets?&#{query_string(*terms)}"
     result = open(path).read
     result.scan(%r{<a href="http://www\.google\.com/search\?hl=en&amp;q=[^"]+">(.*?)</a>}).map {|i| i.to_s}
+  rescue StandardError => e
+    raise GoogleSet::Error.new(e)
   end
 
-private 
+private
   def self.query_string(*terms)
     params = terms.inject({}) do |hash, term|
       hash["q#{hash.size + 1}"] = term
--- spec/google_set_spec.rb
+++ spec/google_set_spec.rb
@@ -8,4 +8,9 @@ describe "GoogleSet" do
   it "should return nothing if no elements are specified" do
     GoogleSet.for().should be_empty
   end
+
+  it "should raise a GoogleSet::Error exception on failure to read from google sets" do
+    GoogleSet.stub!(:open).and_raise(StandardError)
+    lambda{GoogleSet.for("apple", "orange")}.should raise_error GoogleSet::Error
+  end
 end
